e649be4cf3b5cc41560302ab3a851b4c824e7b37
inf03600 <inf03600@adobe.com>
2026-01-22 00:11:36 +0530

    using placeholder

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,16 @@
+import { fetchPlaceholders } from '/scripts/placeholders.js';
+
+
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
+}
+
+export default async function decorate(block) {
+	const placeholders = await fetchPlaceholders();
+  blockquote.append(placeholders);
+
 }
