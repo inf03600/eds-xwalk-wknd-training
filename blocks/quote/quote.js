9101f33b53fc33b833479dc3a1929f017c5ac22d
inf03600 <inf03600@adobe.com>
2026-01-22 00:43:36 +0530

    No change

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,19 +1,7 @@
-import { fetchPlaceholders } from '/scripts/placeholders.js';
-
-
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
-}
-
-export default async function decorate(block) {
-	const placeholders = await fetchPlaceholders();
-  const myQuote = placeholders.quoteText;
-  const blockquotetext = block.querySelector('blockquote');
-  if (blockquote) {
-    blockquote.textContent = myQuote;
-  }
 }
