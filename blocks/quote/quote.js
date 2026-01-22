89c98c2edcc4d8a935591c7e6d2a6bd7cc35ae40
inf03600 <inf03600@adobe.com>
2026-01-23 01:18:00 +0530

    qqq

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -7,16 +7,17 @@ export default async function decorate(block) {
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
 
-  // const { quoteText } = placeholders;
-  // blockquote.append(quoteText);
-  // quoteWrapper.replaceChildren(blockquote);
-    console.log('quote loaded');
-    const placeholders = await fetchPlaceholders();
-    console.log('loaded placeholders',placeholders);
-    if(placeholders.quoteText) {
-        //const suffix = document.createElement('div');
-        //suffix.className='quoteText';
-        quoteWrapper.textContent= placeholders.quoteText;
-        block.appendChild(blockquote);
-    }
+  const { quoteText } = placeholders;
+  blockquote.append(quoteText);
+  quoteWrapper.replaceChildren(blockquote);
+  
+    // console.log('quote loaded');
+    // const placeholders = await fetchPlaceholders();
+    // console.log('loaded placeholders',placeholders);
+    // if(placeholders.quoteText) {
+    //     //const suffix = document.createElement('div');
+    //     //suffix.className='quoteText';
+    //     quoteWrapper.textContent= placeholders.quoteText;
+    //     block.appendChild(blockquote);
+    // }
 }
