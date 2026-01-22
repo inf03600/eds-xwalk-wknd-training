4dc9421723543dbf465580a3c8bc05195c766c9f
inf03600 <inf03600@adobe.com>
2026-01-22 15:10:04 +0530

    Update blockquote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,16 @@
+import { fetchPlaceholders } from '/scripts/placeholders.js';
+const placeholders = await fetchPlaceholders();
+
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
+
+  const { quoteText } = placeholders;
+  blockquote.append(quoteText);
+  div.append(blockquote);
+
 }
+
